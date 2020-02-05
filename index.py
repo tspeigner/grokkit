cb1c120e86a44e4504ffa5b1bc1624fd1d108b61
Bill Shanahan <iamshanahan@gmail.com>
2020-02-05 11:51:24 -0800

    more url paths

--- index.py
+++ index.py
@@ -1,8 +1,23 @@
 from flask import Flask
+
 app = Flask(__name__)
+
+
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/hello")
+def hello2():
+    return "Hello world!"
+
+@app.route("/goodbye")
+def goodbye():
+    return "Goodbye, cruel world!"
+
+@app.route("/fbdp")
+def fbdp():
+    return "Foo Bar Dog Pig!"
 
 
 if __name__ == "__main__":
